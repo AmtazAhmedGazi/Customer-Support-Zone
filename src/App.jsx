f871d0498f7b2e0d9c0a998a7460aaac474affeb
AmtazAhmedGazi <amtazahmedgazi@gmail.com>
2025-11-18 01:47:47 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { ToastContainer } from "react-toastify";
+function App() {
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <ToastContainer />
+    </>
+  );
+}
+
+export default App;
