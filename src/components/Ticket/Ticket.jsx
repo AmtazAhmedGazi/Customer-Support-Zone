fb06c72270faa2268e73a22eecfb0b69f61dd9ce
AmtazAhmedGazi <amtazahmedgazi@gmail.com>
2025-11-21 03:46:31 +0600

    Fixed button

--- src/components/Ticket/Ticket.jsx
+++ src/components/Ticket/Ticket.jsx
@@ -13,56 +13,61 @@ const Ticket = ({ ticket, tasks, setTasks }) => {
   };
 
   return (
-    <div className="max-w-[513px] max-h-[150px] p-4 shadow-sm rounded bg-base-100 mb-4">
-      <div className="flex justify-between mb-2">
-        <h2 className="font-semibold md:text-xl">{ticket.title}</h2>
-        <button
-          disabled={inProgress}
-          onClick={handleOpen}
-          className={`px-2 md:px-3 py-1 rounded-3xl font-semibold flex items-center md:text-xl ${
-            inProgress
-              ? "bg-[#F8F3B9] text-[#9C7700]"
-              : "bg-[#B9F8CF] text-[#0B5E06]"
-          }`}
-        >
-          <div
-            className={`p-2 md:p-2.5 rounded-4xl md:h-3 md:w-3 mr-1.5 ${
-              inProgress ? "bg-[#FEBB0C]" : "bg-[#02A53B]"
-            }`}
-          ></div>
-          {inProgress ? "In-Progress" : "Open"}
-        </button>
-      </div>
-      <p className="text-[#627382] mb-4 text-sm md:text-md">
-        {ticket.description}
-      </p>
-      <div className="flex justify-between">
-        <div className="flex gap-4">
-          <h3 className="text-[#627382] text-sm md:text-md">#00{ticket.id}</h3>
-          <h3
-            className={`text-sm md:text-md ${
-              ticket.priority === "Low"
-                ? "text-[#02A53B]"
-                : ticket.priority === "Medium"
-                ? "text-[#FEBB0C]"
-                : "text-[#F83044]"
+    <div>
+      <div
+        onClick={!inProgress ? handleOpen : undefined}
+        className="max-w-[513px] max-h-[150px] p-4 shadow-sm rounded bg-base-100 mb-4"
+      >
+        <div className="flex justify-between mb-2">
+          <h2 className="font-semibold md:text-xl">{ticket.title}</h2>
+          <button
+            className={`px-2 md:px-3 py-1 rounded-3xl font-semibold flex items-center md:text-xl ${
+              inProgress
+                ? "bg-[#F8F3B9] text-[#9C7700]"
+                : "bg-[#B9F8CF] text-[#0B5E06]"
             }`}
           >
-            {ticket.priority.toUpperCase()} PRIORITY
-          </h3>
+            <div
+              className={`p-2 md:p-2.5 rounded-4xl md:h-3 md:w-3 mr-1.5 ${
+                inProgress ? "bg-[#FEBB0C]" : "bg-[#02A53B]"
+              }`}
+            ></div>
+            {inProgress ? "In-Progress" : "Open"}
+          </button>
         </div>
-        <div className="flex">
-          <h3 className="pr-4 text-[#627382] text-sm md:text-md  md:w-full">
-            {ticket.customer}
-          </h3>
-          <img
-            className="pr-2 w-5 h-5 md:w-10 md:h-full"
-            src={calanderImg}
-            alt=""
-          />
-          <h3 className="text-[#627382] text-sm md:text-md">
-            {ticket.createdAt}
-          </h3>
+        <p className="text-[#627382] mb-4 text-sm md:text-md">
+          {ticket.description}
+        </p>
+        <div className="flex justify-between">
+          <div className="flex gap-4">
+            <h3 className="text-[#627382] text-sm md:text-md">
+              #00{ticket.id}
+            </h3>
+            <h3
+              className={`text-sm md:text-md ${
+                ticket.priority === "Low"
+                  ? "text-[#02A53B]"
+                  : ticket.priority === "Medium"
+                  ? "text-[#FEBB0C]"
+                  : "text-[#F83044]"
+              }`}
+            >
+              {ticket.priority.toUpperCase()} PRIORITY
+            </h3>
+          </div>
+          <div className="flex">
+            <h3 className="pr-4 text-[#627382] text-sm md:text-md  md:w-full">
+              {ticket.customer}
+            </h3>
+            <img
+              className="pr-2 w-5 h-5 md:w-10 md:h-full"
+              src={calanderImg}
+              alt=""
+            />
+            <h3 className="text-[#627382] text-sm md:text-md">
+              {ticket.createdAt}
+            </h3>
+          </div>
         </div>
       </div>
     </div>
